8d75a58f317394a6b6d3a4dd7344fe1d25a96845
Maurício de Sousa Carvalho <mauricio.carvalho@radixeng.com.br>
2021-08-26 16:24:41 -0300

    separando o modal em componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import { GlobalStyle } from './styles/global';
+import { useState } from 'react';
+import Modal from 'react-modal';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard/index';
 
+import { GlobalStyle } from './styles/global';
+import { NewTransactionModal } from './components/NewTransactionModal/index';
+
+Modal.setAppElement('#root');
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsTransactionNewModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsTransactionNewModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsTransactionNewModalOpen(false);
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
+
       <GlobalStyle />
     </>
   );
